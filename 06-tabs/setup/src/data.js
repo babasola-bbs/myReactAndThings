fd24c399ea2431f7a940207b8d020117b72d3cdb
Babasola Oso <easybabasola@gmail.com>
2023-03-20 05:05:58 +0100

    CV using tabs

---
+++ 06-tabs/setup/src/data.js
@@ -0,0 +1,39 @@
+const cv = [
+  {
+    id: "reckhkkylk",
+    order: 3,
+    title: 'Full Stack Web Developer',
+    dates: "December 2015 - Present",
+    duties:[
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+      'sfkjdfjfjfskjfbdjdjke8736378ehhj9wiueurwiweuneoriwrurijrksd,h'
+    ],
+    company: "CreditVille"
+      
+  },
+  {
+    id: "recjrkruffk",
+    order: 2,
+    title: 'Front-End Engineer',
+    dates: "April 2013 - December 2015",
+    duties:[
+      ' Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+      'sfkjdfjfjfskjfbdjdjke8736378ehhj9wiueurwiweuneoriwrurijrksd,h'
+    ],
+    company: "Interswitch"
+     
+  },
+  {
+    id: "recqaeads",
+    order: 1,
+    title: 'Engineering Intern',
+    dates: "February 2012 - March 2013",
+    duties:[
+      ' Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+      'sfkjdfjfjfskjfbdjdjke8736378ehhj9wiueurwiweuneoriwrurijrksd,h'
+    ],
+    company: "Facebook"
+  },
+
+]
+export default cv
